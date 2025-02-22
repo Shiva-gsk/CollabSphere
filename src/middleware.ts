a4bdd6bef13e5977ec66c0124a8bd5a69a3f4fd4
Shiva <shivagulapala2005@gmail.com>
2025-02-23 03:11:50 +0530

    Add Prisma Adapter

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,17 @@
-import NextAuth from "next-auth"// import {auth} from "@/auth";
-import authConfig from "./auth.config"
+// import NextAuth from "next-auth"// import {auth} from "@/auth";
+// import authConfig from "./auth.config"
 
-const {auth} = NextAuth(authConfig);
+// const {auth} = NextAuth(authConfig);
 
-export default auth((req) =>{
-    console.log(req.nextUrl.pathname);
-})
+// export default auth((req) =>{
+//     console.log(req.nextUrl.pathname);
+// })
 
-export const config = {
-    matcher: [
-      // Skip Next.js internals and all static files, unless found in search params
-      '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-      // Always run for API routes
-      '/(api|trpc)(.*)',
-    ],
-  }
+// export const config = {
+//     matcher: [
+//       // Skip Next.js internals and all static files, unless found in search params
+//       '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//       // Always run for API routes
+//       '/(api|trpc)(.*)',
+//     ],
+//   }
